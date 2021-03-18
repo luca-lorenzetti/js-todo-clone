c281a71c1d241bbbc59dde3d0e9463e24da2bc17
luca <luca.lorenzetti0@gmail.com>
2021-03-18 15:58:58 +0100

    Inserita funzione add todo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(function(){
 
+    $('#todo').keydown(function(event){
+        if( event.which === 13){
+           if( $(this).val() != "" ) {
+            var todoTemplate = $('.template li').clone();
+               $(todoTemplate).prepend($(this).val());
+               $('ul').append(todoTemplate);
+               $(this).val("");
+           }
+        }
+    });
+
+    // In ascolto sull'evento click
+    $('.content').on("click", "li button", function(){
+        $(this).parent().remove();
+    });
 
 });
